cf70ebe27dce827089a875483ef06a9b9c6c4d78
Tommi Reiman <tommi@iki.fi>
2015-04-05 14:53:55 +0300

    clean up tests

--- test/scjsv/core_test.clj
+++ test/scjsv/core_test.clj
@@ -4,29 +4,22 @@
             [clojure.java.io :as io]))
 
 (def schema-string (slurp (io/resource "scjsv/schema.json")))
-(def valid (slurp (io/resource "scjsv/valid.json")))
-(def invalid (slurp (io/resource "scjsv/invalid.json")))
 
 (fact "validate-json"
-  (validate-json schema-string valid) => nil
-  (validate-json schema-string invalid) =not=> nil)
+  (let [valid (slurp (io/resource "scjsv/valid.json"))
+        invalid (slurp (io/resource "scjsv/invalid.json"))]
+    (validate-json schema-string valid) => nil
+    (validate-json schema-string invalid) =not=> nil))
 
 (fact "validate"
-  (validate schema-string
-            {:shipping_address
-             {:street_address "1600 Pennsylvania Avenue NW"
-              :city "Washington"
-              :state "DC"}
-             :billing_address
-             {:street_address "1st Street SE"
-              :city "Washington"
-              :state "DC"}}) => nil
-  (validate schema-string
-            {:shipping_address
-             {:street_address "1600 Pennsylvania Avenue NW",
-              :state "DC"}
-             :evil true,
-             :billing_address
-             {:street_address "1st Street SE",
-              :city "Washington",
-              :state "DC"}}) =not=> nil)
+  (let [valid {:shipping_address
+              {:street_address "1600 Pennsylvania Avenue NW"
+               :city "Washington"
+               :state "DC"}
+              :billing_address
+              {:street_address "1st Street SE"
+               :city "Washington"
+               :state "DC"}}
+        invalid (update-in valid [:shipping_address] dissoc :state)]
+    (validate schema-string valid) => nil
+    (validate schema-string invalid) =not=> nil))
