85478e68b05f6323b39a542386bb043ec70fb8f6
Tommi Reiman <tommi@iki.fi>
2015-11-29 19:17:01 +0200

    Fix Java 1.6 build

--- project.clj
+++ project.clj
@@ -8,10 +8,10 @@
   :dependencies [[cheshire "5.5.0"]
                  [com.github.fge/json-schema-validator "2.2.6"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
+                                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
                                   [midje "1.8.2"]]
                    :plugins [[lein-clojars "0.9.1"]
                              [lein-ring "0.9.7"]
                              [lein-midje "3.2"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :aliases {"test-ancient" ["midje"]})
